728397495ace20aad13a3899c008a4e88cdbd572
Gustavo souza <santos.gs708@gmail.com>
2023-05-16 11:00:53 -0300

    adicionando sem certificado ssl

--- resumax/settings.py
+++ resumax/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['resumax.online','www.resumax.online']
+ALLOWED_HOSTS = ['resumax.online','www.resumax.online','localhost', '1270.0.0.1']
 
 
 # Application definition
